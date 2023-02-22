fc857bcc3b602ca6252e485d48e1340bea84e03b
smm1030 <408816223@qq.com>
2023-02-22 04:06:23 -0800

    项目第一天

---
+++ src/components/tab-bar/tab-bar _02.vue
@@ -0,0 +1,54 @@
+ <template>
+  <div class="tab-bar">
+   
+    <div class="tab-bar-item">
+      <img src="@/assets/img/tabbar/tab_favor.png" alt="">
+      <span class="text">收藏</span>
+    </div>
+    <div class="tab-bar-item">
+      <img src="@/assets/img/tabbar/tab_order.png" alt="">
+      <span class="text">订单</span>
+    </div>
+    <div class="tab-bar-item">
+      <img src="@/assets/img/tabbar/tab_message.png" alt="">
+      <span class="text">消息</span>
+    </div>
+
+  </div>
+</template>
+
+<script setup>
+
+
+</script>
+
+<style lang="less" scoped>
+
+.tab-bar {
+  position: fixed;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  height: 50px;
+  display: flex;
+
+  border-top: 1px solid #f3f3f3;
+
+  .tab-bar-item {
+    flex: 1;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+
+    .text {
+      font-size: 12px;
+      margin-top: 2px;
+    }
+
+    img {
+      width: 36px;
+    }
+  }
+}
+</style>
---
+++ src/components/tab-bar/tab-bar _03.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="tab-bar">
+    <van-tabbar v-model="currentIndex" active-color="#ff9854">
+      <template v-for="(item, index) in tabbarData">
+        <van-tabbar-item :to="item.path" icon="home-o">
+          <span>{{  item.text }}</span>
+
+        </van-tabbar-item>
+      </template>
+
+    </van-tabbar>
+
+    <!-- <template v-for="(item, index) in tabbarData">
+      <div
+        class="tab-bar-item"
+        :class="{active: currentIndex === index }"
+        @click="itemClick(index, item)"
+      >
+        <img v-if="currentIndex !== index" :src="getAssetURL(item.image)" alt="">
+        <img v-else :src="getAssetURL(item.imageActive)" alt="">
+        <span class="text">{{ item.text}}</span>
+      </div>
+    </template> -->
+
+  </div>
+</template>
+
+<script setup>
+import tabbarData from "@/assets/data/tabbar.js";
+
+import { getAssetURL } from "@/utils/load_assets.js";
+import { ref } from "vue";
+
+
+const currentIndex = ref(0)
+
+
+</script>
+
+<style lang="less" scoped>
+
+.tab-bar {
+  :deep(.van-tabbar-item__icon) {
+    font-size: 50px;
+  }
+  img {
+    height: 28px;
+  }
+
+}
+</style>
